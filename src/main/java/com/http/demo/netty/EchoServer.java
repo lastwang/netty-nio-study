f7624f49b85bf68d213e0bad385387215c561f34
wangjian <wangjian9@asiainfo.com>
2020-04-07 18:18:53 +0800

    jdk11以及netty的使用

--- src/main/java/com/http/demo/netty/EchoServer.java
+++ src/main/java/com/http/demo/netty/EchoServer.java
@@ -8,7 +8,6 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class EchoServer {
