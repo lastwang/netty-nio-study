13a6376dfef67c92cd183b5ba742e99d634d4939
wangjian <wangjian9@asiainfo.com>
2020-04-20 11:29:14 +0800

    spring+netty

---
+++ src/main/java/com/http/demo/Main.java
@@ -0,0 +1,8 @@
+package com.http.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TestController.main(new String[]{"dada"});
+    }
+}
--- src/main/java/com/http/demo/TestController.java
+++ src/main/java/com/http/demo/TestController.java
@@ -1,5 +1,7 @@
 package com.http.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +17,11 @@ public class TestController {
         System.out.println(atomicInteger.get());
         Thread.sleep(50);
         return "hello undertow";
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+
+    public static void main(String[] args) {
+        log.info("wada,{}", (Object) args);
     }
 }
